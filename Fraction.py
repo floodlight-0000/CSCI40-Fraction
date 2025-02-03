242f08952b45ce3d93a66d499f0f5b2a86aa2cee
khy411 <khy.almojuela@gmail.com>
2025-02-03 20:42:48 +0800

    feat: implement gcd method

--- Fraction.py
+++ Fraction.py
@@ -5,13 +5,10 @@ class Fraction(object):
         pass
 
     def gcd(a, b):
-         def gcd(a, b):
+        if isinstance (a, Fraction):
+            a = a.a
 
-        if a == 0 and b == 0:
-            return 0
-        elif a == 0 and b == 1:
-            return 0
-        elif a == 1 and b == 0:
+        if a == 0 or b == 0:
             return 0
 
         def get_gcd(a,b):
@@ -20,7 +17,7 @@ class Fraction(object):
             return get_gcd(b % a, a)
         
         return get_gcd(a,b)
-
+        
     def get_numerator(self):
         #TODO
         pass
